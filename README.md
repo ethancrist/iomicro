3afb308a40811f490e3010aa65cca10eea596f8b
Ethan Crist <ethancrist96@gmail.com>
2017-12-02 17:27:38 -0500

    Added core functionality

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ A simple, powerful way to create microservices in node.
 ```
 npm install microservice
 ```
-## Active libaries used
-* [express](https://www.npmjs.com/package/express) (latest)
-* [body-parser](https://www.npmjs.com/package/body-parser) (latest)
-* [express-dot-engine](https://www.npmjs.com/package/express-dot-engine)
-* [simple-node-logger](https://www.npmjs.com/package/simple-node-logger)
+#### Active libaries used
+* ###### [express](https://www.npmjs.com/package/express) (latest)
+* ###### [body-parser](https://www.npmjs.com/package/body-parser) (latest)
+* ###### [express-dot-engine](https://www.npmjs.com/package/express-dot-engine)
+* ###### [simple-node-logger](https://www.npmjs.com/package/simple-node-logger)
 
 ## Complete example
 The real beauty behind this library is the ability to set up microservices very quickly, without having to redefine the redundant setup parts of the app.
@@ -21,7 +21,7 @@ micro.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
-micro.listen(3000, { appName: 'My App' }, 'This app is now running.'); 
+micro.listen(3000, { appName: 'My App' hello: 'The app is now online.' }); 
 ```
 This tiny app will do the following:
 * Run an express app on port 3000
@@ -30,12 +30,12 @@ This tiny app will do the following:
 * Render fast, dynamic pages using [dotJS](http://olado.github.io/doT)
 * JSONize POST request paramaters into ```req.body```
 
-## API
+## Usage
 ```javascript
 const micro = require('microservice');
 ```
 
-### Endpoints
+### Creating endpoints
 ```javascript
 micro.get('/api/users', { private: true }, (req, res) => {
     res.json([{ username: 'user1' }]
@@ -47,10 +47,15 @@ micro.post('apps/app', (req, res) => {
 });
 
 ### Logging
+All logging timestamps use the timezone native to your system.
 ```javascript
 micro.log('Log this message please.');
 ```
-Output in console and new line in ```logs/<YYYY>-<MM>-<DD>.log```
-```19:01:24.366 INFO  [<appName>] Log this message please.```
+Outputs in the console and in the file ```logs/<YYYY>-<MM>-<DD>.log``` as
+```
+19:01:24.366 INFO  [<appName>] Log this message please.
+```
 
-This uses the timezone native to your system.
+```javascript
+micro.log('Something bad happened :(', 'error');
+```
--- app.js
+++ app.js
@@ -8,32 +8,82 @@
 
 // [DEPENDENCIES]
 const app = require('express')();
+var log = require('simple-node-logger');
 const bodyParser = require('body-parser');
 const dots = require('express-dot-engine');
-const log = require('simple-node-logger').createRollingFileLogger({
-    logDirectory: 'log/',
-    fileNamePattern: '<DATE>.log',
-    dateFormat: 'YYYY-MM-DD'
-});
+const path = require('path');
+const fs = require('fs');
 
 // [MIDDLEWARE]
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.engine('dot', dots.__express);
 app.set('views', path.join(__dirname, './views'));
-app.use(log.info('[Docs] '+req.method+' '+req.originalUrl+' '+(Object.keys(req.body).length > 0 ? JSON.stringify(req.body)+' ' : '')+req.headers['x-forwarded-for'])
-        next()
-);
+app.use(function(req, res, next) {
+    if (!log.ready) return next();
 
-app.use((req, res, next) => {
-    if (req.headers.authorization !== process.argv[2]) return res.status(403).json({ message: 'Missing proper authorization.' });
-    next();
+    log.info('[Docs] '+req.method+' '+req.originalUrl+' '+(Object.keys(req.body).length > 0 ? JSON.stringify(req.body)+' ' : '')+req.headers['x-forwarded-for'])
+    next()
 });
 
-function log(message) {
-   log.info(message); 
+// [OPTIONS]
+var config = {
+    appName: 'Microservice',
+    logDir: 'log'
+};
+
+// [ESSENTIALS]
+function initLogs() {
+    if (!fs.existsSync(config.logDir)) fs.mkdirSync(config.logDir);
+
+    // TODO: Create 'log/' dir if doesn't exist
+    log = log.createRollingFileLogger({
+        logDirectory: config.logDir+'/',
+        fileNamePattern: '<DATE>.log',
+        dateFormat: 'YYYY-MM-DD'
+    });
+
+    log.ready = true;
+}
+function checkAuth(req, res, next) {
+    if (req.headers.authorization !== process.argv[2]) return res.status(403).json({ message: 'Missing proper authorization.' });
+    next();
+}
+function request(url, options, callback) {
+    if (typeof(options) === 'function') {
+        callback = options;
+        options = null;
+    }
+    if (options.private) checkAuth(callback);
+    
+    if (method === 'GET') app.get(url, callback);
+    if (method === 'POST') app.post(url, callback);
+    if (method === 'PUT') app.put(url, callback);
+    if (method === 'DELETE') app.delete(url, callback);
+}
+var endpoint = {
+    get: function (url, options, callback) { request('GET', url, options, callback },
+    post: function (url, options, callback) { request('POST', url, options, callback },
+    put: function (url, options, callback) { request('PUT', url, options, callback },
+    delete: function (url, options, callback) { request('DELETE', url, options, callback },
+};
+
+function listen(port, options) {
+    //if (!port) return log.error('[Microservice] ERROR: You must specify a port when listening.');
+    
+    config = Object.assign(config, options);
+
+    initLogs();
+    log.info('['+config.appName+'] '+config.hello);
+
+    app.listen(port, options.callback);
 }
 
 module.exports = {
-    log: log 
+    log: log.info,
+
+    // Express
+    get: endpoint.get,
+    use: app.use,
+    listen: listen
 };
