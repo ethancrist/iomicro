d2fda0e1c5fe5390d6a90420e75c2e42a6e8dd4d
ethancrist <ethancrist96@gmail.com>
2017-12-03 05:11:22 +0000

    Fixed bug that crashed with no options

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function request(method, url, options, callback) {
 
     // Calling this function here to access req, res, next 
     function callback(req, res, next) {
-        if (options.private) checkAuth(req, res, next);
+        if (options && options.private) checkAuth(req, res, next);
     }
     
     if (method === 'GET') app.get(url, callback);
