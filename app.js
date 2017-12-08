e364fa67e963f793fcf4bec303fd2c403c75440e
ethancrist <ethancrist96@gmail.com>
2017-12-08 13:14:46 +0000

    Update all of master

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function logger(req, res) {
         post: Object.keys(req.body).length > 0 ? JSON.stringify(req.body)+' ' : ''
     };
 
-    log.info('['+config.appName+'] '+res.statusCode+' '+req.method+' '+req.originalUrl+' '+user.post+user.ip);
+    log.info('['+config.appName+'] '+req.method+' '+res.statusCode+' '+req.originalUrl+' '+user.post+user.ip);
 }
 function request(method, url, options, callback) {
     if (!app.ready) prepare();
@@ -104,6 +104,8 @@ function request(method, url, options, callback) {
 
             if (!isAuthorized) response = arguments[1].status(403).json({ message:'Missing proper authorization.' });
         }
+
+        console.log(response);
 
         logger(req, res);
 
